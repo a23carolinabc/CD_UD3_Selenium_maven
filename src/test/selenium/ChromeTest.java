63ca806c42c88a607092560642811d38d54b0908
Carolina <a23carolinabc@iessanclemente.net>
2024-01-23 13:36:50 +0100

    Martes23

---
+++ src/test/selenium/ChromeTest.java
@@ -0,0 +1,15 @@
+package test.selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ChromeTest {
+
+    ChromeDriver driver;
+
+    @Test
+    public void test(){
+        ChromeOptions options = new ChromeOptions();
+
+    }
+}
