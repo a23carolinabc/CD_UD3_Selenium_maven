54499ace18859b28954e42c18080609723908e42
Carolina <a23carolinabc@iessanclemente.net>
2024-01-30 13:01:26 +0100

    Martes 30

--- src/test/java/dev/selenium/ChromeTest.java
+++ src/test/java/dev/selenium/ChromeTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -54,6 +56,7 @@ public class ChromeTest {
         Thread.sleep(2000);
         assertTrue(check.isSelected());
         //assertFalse(check.isSelected()); así saldría la prueba verificada porque ya viene marcado por defecto en la pag
+        driver.quit();
     }
 
     @Test
@@ -64,7 +67,36 @@ public class ChromeTest {
         Thread.sleep(2000);
         WebElement disabledInput = driver.findElement(By.name("my-disabled"));
         disabledInput.sendKeys("carolina");
+        driver.quit();
+        
     }
 
+    @Test
+    public void elementUploadForms() throws Exception {
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+        driver.get("https://the-internet.herokuapp.com/upload");
+        /* Declaramos una variable String con el nombre de la imagen que queremos subir para
+         * que si en el futuro queremos cambiar de imagen solo tegamos que cambiar su nombre
+         * y no la ruta relativa entera, el assertEquals... */
+        String nameImage = "medusa.png";
+        File uploadFile = new File("src/test/files/"+nameImage);
+        WebElement fileInput = driver.findElement(By.cssSelector("input[type=file]"));
+        fileInput.sendKeys(uploadFile.getAbsolutePath());
+        Thread.sleep(1000);
+        /* No se pone directamente la ruta absoluta porque sería única para este ordenador,
+         * si quisiesemos hacer la prueba en otro ordenador dará problemas*/
+        
+         driver.findElement(By.id("file-submit")).click();
+        /* Sería lo mismo que lo de abajo pero sin declarar una variable que no vamos a usar
+        WebElement buttomUpload = driver.findElement(By.id("file-submit"));
+        buttomUpload.click(); */
+        Thread.sleep(2000);
+        //driver.quit();
+        WebElement fileUpload = driver.findElement(By.id("uploaded-files"));
+        assertEquals(nameImage, fileUpload.getText());
+        /* Al poner como lo esperado el nameImage y no el nombre de la imagen escrito a mano evitamos
+         * "Literales", que a futuro complican las pruebas */
+    }
     
 }
